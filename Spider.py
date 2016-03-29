d9c92904425124a1a4ea6e85a80c632c9e8deb8b
R0uter <lomyr.jo@gmail.com>
2016-03-29 19:41:10 +0800

    。。

--- Spider.py
+++ Spider.py
@@ -4,14 +4,13 @@ import IO
 
 #爬虫类
 class Spider:
-    __domainSeeds = []
-    __linkList = []
+    __domainList = []
 
     def __init__(self):
 
         self.__reviewer = Reviewer.Reviewer().startReviewer()
         self.__io = IO.IO.getIO()
-        self.__domainSeeds = self.__getSeeds()
+        # self.__domainSeeds = self.__getSeeds()
 
 
 
@@ -22,10 +21,13 @@ class Spider:
         pass
 #准备种子,如果是第一次启动则使用字面量启动
     def __getSeeds(self):
-        pass
+        return ['www.hao123.com','hao360.cn']
     #获取链接列表里的下一个页面
-    def __nextWebPage(self):
-        pass
+    def __nextPage(self):
+        if len(self.__domainList) == 0:
+            self.__domainList = self.__getSeeds()
+        self.__domainList [0]
+
 #从页面里获取链接
     def __gatherLinksFromPage(self):
         pass
